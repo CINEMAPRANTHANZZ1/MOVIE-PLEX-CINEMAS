9dc3ed9ac4b683cb6a868b4dc014141ab08e147f
MICLE JACKSON <105645220+CINEMAPRANTHANZZ1@users.noreply.github.com>
2022-05-24 16:40:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DHANANJAY4532/PROFESSOR-BOT.git /PROFESSOR-BOT
+  git clone https://github.com/CINEMAPRANTHANZZ1/MOVIE-PLEX-CINEMAS.git /PROF
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PROFESSOR-BOT
+  git clone $UPSTREAM_REPO /MOVIE-PLEX-CINEMAS
 fi
 cd /PROFESSOR-BOT
 pip3 install -U -r requirements.txt
-echo "Starting 𝙼𝙺𝙽 𝙱𝙾𝚃𝚉....🔥"
+echo "Starting MOVIE PLEX CINEMAS"
 python3 bot.py
